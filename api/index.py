072f9a10382a01979a3496a32eb5de9d28a9d5ac
SvenUythoven <sven.uythoven@students.fhnw.ch>
2023-12-13 11:51:38 +0100

    liste in api

--- api/index.py
+++ api/index.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
+
+@app.get("/api/list")
+def liste():
+    return {"liste": ["Apfel", "Banane", "Birne", "Ananas", "Orange"]}
